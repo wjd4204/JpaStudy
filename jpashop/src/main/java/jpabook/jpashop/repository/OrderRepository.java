0773108ad1dbd3e648c1ea967df8a8c7484dc89e
wjd4204 <brian876373@gmail.com>
2024-08-31 13:05:28 +0900

    feat: 상품 주문 및 목록 검색과 취소 구현

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -25,7 +25,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch){
+    public List<Order> findAllByString(OrderSearch orderSearch){
 
         String jpql = "select o from Order o join o.member m";
         boolean isFirstCondition = true;
