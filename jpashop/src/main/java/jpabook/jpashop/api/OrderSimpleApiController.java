8160434f656af8823230e538845508500e01630f
wjd4204 <brian876373@gmail.com>
2024-09-13 00:08:41 +0900

    Section4: 엔티티를 DTO로 변환-페치 조인 사용

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -44,6 +44,16 @@ public class OrderSimpleApiController {
                 .toList();
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3(){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .toList();
+
+        return result;
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
