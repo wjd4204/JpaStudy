60d933ec7c1c9cd9d3cbbc785c537c736228e6a5
wjd4204 <brian876373@gmail.com>
2024-11-04 14:39:00 +0900

    section 6. Auditing 구현

--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -4,9 +4,11 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import java.util.List;
 
@@ -19,6 +21,8 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     public void testEntity(){
@@ -49,5 +53,28 @@ class MemberTest {
             System.out.println("-> member.team = " + member.getTeam());
         }
 
+    }
+
+    @DisplayName("")
+    @Test
+    void JpaEventBaseEntity() throws InterruptedException {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.updatedDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.createdBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.updatedBy = " + findMember.getLastModifiedBy());
     }
 }
