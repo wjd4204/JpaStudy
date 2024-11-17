cc0032ac0cc5cb519a93624a270d1405915919e5
wjd4204 <brian876373@gmail.com>
2024-11-17 17:48:08 +0900

    Section 4. 정렬

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -110,7 +110,7 @@ public class QuerydslBasicTest {
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
-    @DisplayName("")
+    @DisplayName("여러가지 결과 조회를 테스트해본다.")
     @Test
     void resultFetch(){
      //given
@@ -137,5 +137,35 @@ public class QuerydslBasicTest {
                 .selectFrom(member)
                 .fetchCount();
 
+    }
+
+    /*
+    회원 정렬 순서
+    1. 회원 나이 내림차순
+    2. 회원 이름 올림차순
+    2에서 회원 이름이 없으면 마지막에 null 출력
+     */
+    @DisplayName("회원들을 특정 속성을 베이스로 정렬한다.")
+    @Test
+    void sort(){
+     //given
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        //when
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+     //then
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
     }
 }
