2ed06bd593fa4fd05303d03eeeeacbab042a62c9
wjd4204 <brian876373@gmail.com>
2024-12-04 16:48:33 +0900

    Section 6. 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,10 +1,11 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -13,6 +14,7 @@ import study.querydsl.entity.Member;
 import java.util.List;
 import java.util.Optional;
 
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
@@ -62,10 +64,10 @@ public class MemberJpaRepository {
     public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition){
 
         BooleanBuilder builder = new BooleanBuilder();
-        if (StringUtils.hasText(condition.getUsername())) { // 넘어온 값에 대해 ""가 아니거나 올바론 텍스트 형식을 갖지 않을 때 사용
+        if (hasText(condition.getUsername())) { // 넘어온 값에 대해 ""가 아니거나 올바론 텍스트 형식을 갖지 않을 때 사용
             builder.and(member.username.eq(condition.getUsername()));
         }
-        if (StringUtils.hasText(condition.getTeamName())) {
+        if (hasText(condition.getTeamName())) {
             builder.and(team.name.eq(condition.getTeamName()));
         }
         if (condition.getAgeGoe() != null) {
@@ -85,6 +87,56 @@ public class MemberJpaRepository {
                 )
                 .from(member)
                 .where(builder)
+                .leftJoin(member.team, team)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search (MemberSearchCondition condition){
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName"))
+                )
+                .from(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .leftJoin(member.team, team)
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.eq(ageLoe) : null;
+    }
+
+    // BooleanExpression은 재사용이 가능하다!
+    public List<Member> searchMember (MemberSearchCondition condition){
+        return queryFactory
+                .selectFrom(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
                 .leftJoin(member.team, team)
                 .fetch();
     }
